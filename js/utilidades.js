32916b3c912ff0876beea10f8e4f80c0275d4dc6
HectorAgHe <aguilarhector440@gmail.com>
2022-10-23 13:54:44 -0500

    Validando login

---
+++ js/utilidades.js
@@ -0,0 +1,12 @@
+//funciones que utilizan ambas clases -registro e index
+
+function creaMensaje(texto, tipo){
+    const nuevoElemento = document.createElement('div');
+    nuevoElemento.innerText = texto;
+    nuevoElemento.classList.add('alert','alert-'+ tipo);
+    const divMensaje = document.getElementById('mensaje');
+    divMensaje.appendChild(nuevoElemento);
+    setTimeout(function(){
+        nuevoElemento.remove();
+    }, 2000)
+}
